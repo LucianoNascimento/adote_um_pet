8ef23117b1c5365e20958374f485709e3490e714
LucianoNascimento <dev5@ezsoft.com.br>
2024-10-28 23:16:23 -0300

    nomeando as rotas

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 
-Route::post('/register', [RegisterController::class, 'register']);
-Route::post('login', [LoginController::class, 'login']);
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
+Route::post('login', [LoginController::class, 'login'])->name('login');
 Route::post('logout', [LoginController::class, 'logout'])->middleware('auth:sanctum');
 
 
